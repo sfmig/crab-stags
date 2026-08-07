42f39630498e81986490b8738b5e896fe1549a0e
sfmig <33267254+sfmig@users.noreply.github.com>
2026-08-07 12:47:10 +0100

    Caliscope for intrinsic calibration

---
+++ notebook_calibrate_intrinsic_one_camera.py
@@ -0,0 +1,61 @@
+"""
+Reference: https://github.com/mprib/caliscope/blob/main/scripts/demo_api.py
+
+If we do this via the GUI, we need to have at least 3 videos in intrinsics and extrinsics I think
+"""
+# %%
+from pathlib import Path
+
+from caliscope.api import (
+    CameraArray,
+    Charuco,
+    CharucoTracker,
+    calibrate_intrinsics,
+    extract_image_points,
+)
+
+# %%
+# Params
+video_path = Path(
+    "/Users/sofia/swc/project_caliscope/P2/calibration/intrinsic/cam_2.mp4"
+)
+
+charuco_n_cols = 4
+charuco_n_rows = 5
+charuco_square_sz_cm = 5.40  # default 3.0?
+
+# %%
+# Create charuco tracker
+charuco = Charuco.from_squares(
+    columns=charuco_n_cols,
+    rows=charuco_n_rows,
+    square_size_cm=charuco_square_sz_cm,
+) 
+tracker = CharucoTracker(charuco)
+
+# Initialise camera
+# (gets pixel resolution from video)
+cameras = CameraArray.from_video_metadata({0: video_path})
+
+# set Fisheye lens
+# fisheye model uses 4 distortion coefficients (k1, k2, k3, k4);
+# standard uses 5
+cameras[0].fisheye = True   # set False for normal lenses
+
+# Extract 2d landmarks from calibration video
+points = extract_image_points(
+    video_path,
+    cam_id=0,
+    tracker=tracker,
+    frame_step=30,
+)
+
+# Calibrate single camera
+output = calibrate_intrinsics(points, cameras[0])
+
+# %%
+print(output.camera.matrix)
+print(output.camera.distortions)
+print(f"RMSE (px): {output.camera.error}")
+# output.report
+# %%
